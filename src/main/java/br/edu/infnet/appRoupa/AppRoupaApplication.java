a988a6c9ce9fe260b878b33dc2881ac1855e2ab6
Robson <robson.borges@al.infnet.edu.br>
2020-12-15 12:50:50 -0300

    Inicio do projeto appRoupa

---
+++ src/main/java/br/edu/infnet/appRoupa/AppRoupaApplication.java
@@ -0,0 +1,14 @@
+package br.edu.infnet.appRoupa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRoupaApplication {
+
+	public static void main(String[] args) {
+//		SpringApplication.run(AppRoupaApplication.class, args);
+		System.out.println(args.length);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/appRoupa/AppRoupaApplicationTests.java
@@ -0,0 +1,14 @@
+package br.edu.infnet.appRoupa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRoupaApplicationTests {
+
+	@Test
+	void testeIncial() {
+		AppRoupaApplication.main(new String[] {"robson"});
+	}
+
+}
